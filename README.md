20ba51746daa084979dbcfb06793b364b33366fa
Dan Gamble <dangamble89@gmail.com>
2016-02-06 16:18:32 +0000

    Added the replacement option which is a much more involved way to use Font Awesome

--- README.md
+++ README.md
@@ -10,17 +10,64 @@
 [ci-img]:  https://travis-ci.org/dan-gamble/postcss-font-awesome.svg
 [ci]:      https://travis-ci.org/dan-gamble/postcss-font-awesome
 
+#### Before
 ```css
 .foo::before {
   font-awesome: camera;
 }
 ```
 
+#### After
 ```css
 .foo::before {
+  font-family: FontAwesome;
   content: '\f030';
 }
 ```
+
+## Options
+
+### Replacement
+By default the plugin just searches for the icon you want and adds the Font Awesome font family as well as the unicode characters relative to that icon inside a `content: `.
+
+With the replacement option you can use it as a full class replacement.
+
+#### Before
+```css
+.foo {
+  font-awesome: camera;
+}
+```
+
+#### After
+```css
+.foo {
+  display: inline-block;
+  font: normal normal normal FontAwesome;
+  font-size: inherit;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.foo::before {
+  font-family: FontAwesome;
+  content: '\f030';
+}
+```
+
+This means you won't have to add have `<i class="fa fa-camera"></i>` you can just use `<i class="foo"></i>`.
+
+There is 1 downside to this method though, it creates a bit more CSS duplication as
+```css
+  display: inline-block;
+  font: normal normal normal FontAwesome;
+  font-size: inherit;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+```
+
+Gets added everytime you use it instead of being on a single `.fa` class.
 
 ## Usage
 
--- test.js
+++ test.js
@@ -12,11 +12,31 @@ function run(t, input, output, opts = { }) {
 }
 
 test('converts icon name into unicode', t => {
-    return run(t, 'a{ font-awesome: camera }', 'a{ content: \'\f030\' }');
+    return run(t, 'a{ font-awesome: camera }', 'a{ font-family: FontAwesome; content: \'\f030\' }');
 });
 
-test('converts icon name into unicode', t => {
-    return run(t, 'a{ font-awesome: facebook }', 'a{ content: \'\f09a\' }');
+test('converts icon name into unicode with comma seperated selectors', t => {
+    return run(t, 'a, b{ font-awesome: camera }', 'a, b{ font-family: FontAwesome; content: \'\f030\' }');
+});
+
+test('options', t => {
+    return run(
+        t,
+        'a{ font-awesome: camera }',
+        'a{ -moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; text-rendering: auto; font-size: inherit; font: normal normal normal FontAwesome; display: inline-block }\na::before{ content: \'\f030\' }', { // eslint-disable-line max-len
+            replacement: true
+        }
+    );
+});
+
+test('options with comma seperated selectors', t => {
+    return run(
+        t,
+        'a, b{ font-awesome: camera }',
+        'a, b{ -moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; text-rendering: auto; font-size: inherit; font: normal normal normal FontAwesome; display: inline-block }\na::before, b::before{ content: \'\f030\' }', { // eslint-disable-line max-len
+            replacement: true
+        }
+    );
 });
 
 test('if icon name doesn\'t exist just use the input value', t => {
