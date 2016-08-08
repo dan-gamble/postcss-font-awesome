e6902b5a1c1ba1af3c0f9e852f75f561eabd982f
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-08-08 15:03:39 +0200

    Use plain https module for post-install script

--- generateIconConfig.js
+++ generateIconConfig.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var fs = require('fs');
-var req = require('request');
+var https = require('https');
 
 var config = {
     src: path.resolve(
@@ -9,25 +9,44 @@ var config = {
     dest: './icons.json'
 };
 
-req.get('https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/scss/_variables.scss', function (e, d, data) {
-    if (e) console.log(e);
+var varsUrl = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/';
+varsUrl += 'master/scss/_variables.scss';
 
-    var lines = data.split('\n').filter(n => {
-        return n !== undefined && n !== '' && n.startsWith('$fa-var-');
-    });
-    var icons = {};
-
-    for (var line of lines) {
-        var icon = line.substring(0, line.indexOf(':')).replace('$fa-var-', '');
-        var code = line.substring(line.indexOf('\\f'), line.indexOf('";'))
-            .replace('\\f', '');
-
-        icons[icon] = code;
+https.request(varsUrl, function (res) {
+    if (res.statusCode !== 200) {
+        throw new Error(
+            'Failed to fetch variables from Github, got HTTP ' + res.statusCode
+        );
     }
 
-    fs.writeFile(config.dest, JSON.stringify(icons), function (error) {
-        if (error) console.log(error);
-
-        console.log('Icons file created');
+    var data = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        data += chunk;
     });
-});
+
+    res.on('end', function () {
+        var lines = data.split('\n').filter(n => {
+            return n !== undefined && n !== '' && n.startsWith('$fa-var-');
+        });
+        var icons = {};
+
+        for (var line of lines) {
+            var icon = line.substring(0, line.indexOf(':'))
+                    .replace('$fa-var-', '');
+
+            var code = line.substring(line.indexOf('\\f'), line.indexOf('";'))
+                    .replace('\\f', '');
+
+            icons[icon] = code;
+        }
+
+        fs.writeFile(config.dest, JSON.stringify(icons), function (error) {
+            if (error) {
+                throw error;
+            }
+
+            console.log('Icons file created');
+        });
+    });
+}).end();
